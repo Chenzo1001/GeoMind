2d47d5d8b5d1611cd64067dab73c017ca0f0f38e
Chenzo <120361592+Chenzo1001@users.noreply.github.com>
2025-03-11 15:51:48 +0800

    Using DeepSeek API

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+deepseek_api_key = st.text_input("DeepSeek API Key", type="password")
+if not deepseek_api_key:
+    st.info("Please add your DeepSeek API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = openai.Client(api_key=deepseek_api_key, base_url="https://api.deepseek.com")
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -40,8 +40,8 @@ else:
             st.markdown(prompt)
 
         # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+        stream = client.completions.create(
+            model="deepseek-chat",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
