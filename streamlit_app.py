506272251ce0744d13aecee545211b235a9adf67
Chenzo <120361592+Chenzo1001@users.noreply.github.com>
2025-03-11 16:00:17 +0800

    QWEN model

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ if not deepseek_api_key:
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=deepseek_api_key, base_url="https://api.deepseek.com")
+    client = OpenAI(api_key=deepseek_api_key, base_url=https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -41,7 +41,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="deepseek-chat",
+            model="qwen-plus",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
